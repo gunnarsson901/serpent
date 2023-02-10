eacae4f95cd0019258cfda880649f53c66ea0ede
Felix <gunnarsson901@gmail.com>
2023-02-10 02:06:56 +0000

    add canvas colors and create interval for each frame

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,32 @@
+// Canvas
 const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+
+// Frame rate
 const frames = 1000 / 15;
-canvas.style.border = "10px solid black";
+
+// Canvas style
+canvas.style.border = "10px solid green";
+const bgColor = '#FFFFFF';
+
+// Canvas dinensions
+const width = canvas.width;
+const height = canvas.height;
+
+// Runs while game is running
+let gameLoop;
+
+function drawBoard() {
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, width, height);
+}
+
+function frame(){
+    drawBoard();
+    // drawFood();
+    // moveSnake();
+    // drawSnake();
+    // displayScore();
+}
+
+gameLoop = setInterval(frame, frames);
