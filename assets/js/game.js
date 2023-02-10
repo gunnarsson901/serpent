52e5482fe24af42079e51aa6c0044dbdc210ed47
Felix <gunnarsson901@gmail.com>
2023-02-10 02:25:54 +0000

    add function to draw squares

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,9 @@ const frames = 1000 / 15;
 canvas.style.border = "10px solid green";
 const bgColor = '#FFFFFF';
 
+// Snake style
+const Sbody = "#F02010"
+
 // Canvas dinensions
 const width = canvas.width;
 const height = canvas.height;
@@ -16,13 +19,28 @@ const height = canvas.height;
 // Runs while game is running
 let gameLoop;
 
+const square = 20;
+
 function drawBoard() {
     ctx.fillStyle = bgColor;
     ctx.fillRect(0, 0, width, height);
 }
 
-function frame(){
+function drawSquare(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * square, y * square, square, square);
+    ctx.strokeStyle = bgColor;
+    ctx.strokeRect(x * square, y * square, square, square); 
+}
+
+function drawFood() {
+
+}
+
+// Loops through game functions
+function frame() {
     drawBoard();
+    drawSquare(10, 0, Sbody)
     // drawFood();
     // moveSnake();
     // drawSnake();
